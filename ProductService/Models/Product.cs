ee1b981abd2fff1c1a03e766c31bfc5bbc673594
mercally1 <31991854+mercally1@users.noreply.github.com>
2024-10-29 21:53:47 -0600

    Add Migrations

--- ProductService/Models/Product.cs
+++ ProductService/Models/Product.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ProductService.Models;
 
 public class Product
 {
+    [Key]
     public int Id { get; set; }
 
+    [Required]
     public string? Name { get; set;  }
 
+    [Required]
     public decimal Price { get; set; }
 }
--- ProductService/Program.cs
+++ ProductService/Program.cs
@@ -24,6 +24,8 @@ internal class Program
         builder.Services.AddDbContext<ProductDbContext>(options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+        builder.Services.AddControllers();
+
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
@@ -34,32 +36,9 @@ internal class Program
         }
 
         app.UseHttpsRedirection();
-
-        var summaries = new[]
-        {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-        app.MapGet("/weatherforecast", () =>
-        {
-            var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-                .ToArray();
-            return forecast;
-        })
-        .WithName("GetWeatherForecast")
-        .WithOpenApi();
+        
+        app.MapControllers();
 
         app.Run();
     }
-}
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
